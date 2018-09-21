88bcdb06d004ff643758e64362fe5f598fdabb1c
pneto97 <paulo_neto@mail.com>
2018-09-21 02:13:39 -0300

    botao de trocar activities de main para new adventure

--- app/src/main/java/br/unb/appdev/igor/MainActivity.java
+++ app/src/main/java/br/unb/appdev/igor/MainActivity.java
@@ -1,54 +1,37 @@
 package br.unb.appdev.igor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     String msg = "Android : ";
+    private ImageButton new_adventure_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(msg, "The onCreate() event");
-    }
+        Log.d(msg, "Evento onCreate()");
 
-    /** Called when the activity is about to become visible. */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(msg, "The onStart() event");
-    }
-
-    /** Called when the activity has become visible. */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(msg, "The onResume() event");
-    }
-
-    /** Called when another activity is taking focus. */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(msg, "The onPause() event");
-    }
-
-    /** Called when the activity is no longer visible. */
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(msg, "The onStop() event");
-    }
-
-    /** Called just before the activity is destroyed. */
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(msg, "The onDestroy() event");
+        new_adventure_button = (ImageButton) findViewById(R.id.button_new_adventure);
+        new_adventure_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewAdventureActivity();
+            }
+        });
     }
 
 
+    public void openNewAdventureActivity() {
+
+        Intent intent = new Intent(this, NewAdventureActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/br/unb/appdev/igor/NewAdventureActivity.java
@@ -0,0 +1,13 @@
+package br.unb.appdev.igor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAdventureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_adventure);
+    }
+}
